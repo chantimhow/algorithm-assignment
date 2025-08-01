5c6846778ed17dbbb4ad0ed0aa473d5ba8ac9fb5
chantimhow <pixelsgt@gmail.com>
2025-08-02 01:34:51 +0800

    more changes

--- Algo Assignment/app.cpp
+++ Algo Assignment/app.cpp
@@ -8,6 +8,9 @@
 #include    "LibStudent.h"
 #include    "LibBook.h"
 #include	<string>
+#include	<ctime>
+#include	<iomanip>
+
 
 
 
@@ -17,17 +20,31 @@ using namespace std;
 
 bool ReadFile(string filename, List* list);
 bool DeleteRecord(List* list, char* id);
-//bool Display(List, int, int);
-bool InsertBook(string filename, List* list);
+bool Display(List* list, int source, int detail);
+bool InsertBook(string filename, List* list, Date currentdate);
 bool SearchStudent(List* list, char* id, LibStudent& studentinfo);
 //bool computeAndDisplayStatistics(List *);
 //bool printStuWithSameBook(List *, char *);
 //bool displayWarnedStudent(List *, List *, List *);
 //int menu();
+time_t convertDate(Date time);
+int compareDate(Date date1, Date date2);
+
+
+
+
+
+
+;
+
+
 
 
 int main() {
 	
+
+	Date currentdate;
+	currentdate.day = 29; currentdate.month = 3; currentdate.year = 2020;
 	cout << "test";
 	cout << "\n\n";
 	system("pause");
@@ -37,20 +54,11 @@ int main() {
 	string filename = "book.txt";
 		if (ReadFile("student.txt", libdata)) cout << "read successful!\n";
 		else cout << "unable to read!";
-		for (int i = 1; i <=  libdata->count; i++) cout << libdata->find(i)->item.name << endl;
+	InsertBook("book.txt", libdata, currentdate);
+	Display(libdata, 1, 1);
+		
 
-		int* x = new int;
-		char* userinput = new char[20];
-		strcpy(userinput, "120023311");
-		char* userinput1 = new char[20];
-		strcpy(userinput1, "1201237");
-		if (DeleteRecord(libdata, userinput)) cout << "deleted successfull!\n";
-		else cout << "delete unsucessfull!\n";
-		for (int i = 1; i <= libdata->count; i++) cout << libdata->find(i)->item.name << endl;
-		if (SearchStudent(libdata, userinput1, currstudent)) cout << "student found\n";
-		else cout << "student not found\n";
-		cout << currstudent.name;
-	/*InsertBook(filename,libdata);*/
+		
 
 
 }
@@ -140,16 +148,17 @@ bool SearchStudent(List* list, char* id, LibStudent &studentinfo) {
 	return false;
 
 }
-bool InsertBook(string filename, List* list) {
+bool InsertBook(string filename, List* list, Date currentdate) {
 	int delicount = 0;
 	int index = 0;
 	ifstream infile;
 	infile.open(filename);
+	if (!infile.is_open()) return false;
 	char tempostring[60];
 	while (infile >> tempostring) {//id
-		for (int i = 1; i < list->count; i++) {
-			if (list->find(i)->item.id == tempostring);
-			index = i;
+		if (tempostring[0] == '\0') continue;
+		for (int i = 1; i <= list->count; i++) {
+			if (list->find(i)->item.id == tempostring) index = i;
 		}
 		infile >> tempostring;//author
 		for (char c : tempostring) {
@@ -172,6 +181,10 @@ bool InsertBook(string filename, List* list) {
 		list->find(index)->item.book[list->find(index)->item.totalbook].due.day = atoi(strtok(tempostring, "/"));
 		list->find(index)->item.book[list->find(index)->item.totalbook].due.month = atoi(strtok(NULL, "/"));
 		list->find(index)->item.book[list->find(index)->item.totalbook].due.year = atoi(strtok(NULL, "/"));
+		list->find(index)->item.book[list->find(index)->item.totalbook].fine = (compareDate(currentdate, list->find(index)->item.book[list->find(index)->item.totalbook].due))*0.5;
+		list->find(index)->item.totalbook++;
+		list->find(index)->item.calculateTotalFine();
+		
 
 	}
 	
@@ -180,7 +193,125 @@ bool InsertBook(string filename, List* list) {
 	
 	return true;
 }
+time_t convertDate(Date time) {
+	struct tm tm_time;
+	tm_time.tm_year = time.year - 1900;
+	tm_time.tm_mon = time.month - 1;
+	tm_time.tm_mday = time.day;
+	tm_time.tm_hour = 0;
+	tm_time.tm_min = 0;
+	tm_time.tm_sec = 0;
+	return mktime(&tm_time);
+
+}
+int compareDate(Date date1, Date date2) {
+	time_t currentdate = convertDate(date1);
+	time_t duedate = convertDate(date2);
+	if (currentdate < duedate) return 0;
+	
+	double diff = difftime(currentdate, duedate);
+	return diff /(60 * 60 * 24);
+
+}
+
+bool Display(List* list, int source, int detail) {
+	ofstream outfile;
+
+	switch (source) {
+	case 1:
+		
+		switch (detail) {
+		case 1:
+			outfile.open("student1.txt");
+			if (!outfile.is_open()) return false;
+			for (int i = 1; i <= list->count; i++) {
+				outfile << endl << "STUDENT " + i << endl;
+				outfile << endl << "Name:  " << list->find(i)->item.name << endl;
+				outfile << "Id:  " << list->find(i)->item.id << endl;
+				outfile << "Course:  " << list->find(i)->item.course << endl;
+				outfile << "Phone No:  " << list->find(i)->item.phone_no << endl;
+				outfile << "Total Fine:  RM" << list->find(i)->item.calculateTotalFine() << fixed << setprecision(2) << endl << endl;
+				outfile << "BOOK LIST:";
+				for (int j = 0; j < list->find(i)->item.totalbook; j++) {
+					outfile << endl << endl << "Book " << j + 1 << endl << endl;
+					outfile << "Title: " << list->find(i)->item.book[j].title << endl;
+					outfile << "Author: " << list->find(i)->item.book[j].author << endl;
+					outfile << "Publisher: " << list->find(i)->item.book[j].publisher << endl;
+					outfile << "Year Published: " << list->find(i)->item.book[j].yearPublished << endl;
+					outfile << "ISBN: " << list->find(i)->item.book[j].ISBN << endl;
+					outfile << "Call Number: " << list->find(i)->item.book[j].callNum << endl;
+					outfile << "Borrow Date: " << list->find(i)->item.book[j].borrow.day << '/' << list->find(i)->item.book[j].borrow.month << '/' << list->find(i)->item.book[j].borrow.year << endl;
+					outfile << "Due Date: " << list->find(i)->item.book[j].due.day << '/' << list->find(i)->item.book[j].due.month << '/' << list->find(i)->item.book[j].due.year << endl;
+					outfile << "Fine: " << list->find(i)->item.book[j].fine << endl;
+				}
+				outfile << "*" << setw(100) << setfill('*');
+				
+			}
+			break;
+					
+		case 2:
+			outfile.open("student1.txt"); 
+			if (!outfile.is_open()) return false;
+			for (int i = 1; i <= list->count; i++) {
+				outfile << endl << "STUDENT " + i << endl;
+				outfile << endl << "Name:  " << list->find(i)->item.name << endl;
+				outfile << "Id:  " << list->find(i)->item.id << endl;
+				outfile << "Course:  " << list->find(i)->item.course << endl;
+				outfile << "Phone No:  " << list->find(i)->item.phone_no << endl;
+				outfile << "Total Fine:  RM" << list->find(i)->item.calculateTotalFine() << fixed << setprecision(2) << endl << endl;
+				outfile << "*" << setw(100) << setfill('*') << endl;
+				
+			}
 			
+			outfile.close();
+			break;
+		}
+		break;
+		
+	case 2:
+		switch (detail) {
+		case 1:
+			for (int i = 1; i <= list->count; i++) {
+				cout << endl << "STUDENT " + i << endl;
+				cout << endl << "Name:  " << list->find(i)->item.name << endl;
+				cout << "Id:  " << list->find(i)->item.id << endl;
+				cout << "Course:  " << list->find(i)->item.course << endl;
+				cout << "Phone No:  " << list->find(i)->item.phone_no << endl;
+				cout << "Total Fine:  RM" << list->find(i)->item.calculateTotalFine() << fixed << setprecision(2) << endl << endl;
+				cout << "BOOK LIST:";
+				for (int j = 0; j < list->find(i)->item.totalbook; j++) {
+					cout << endl << endl << "Book " << j + 1 << endl << endl;
+					cout << "Title: " << list->find(i)->item.book[j].title << endl;
+					cout << "Author: " << list->find(i)->item.book[j].author << endl;
+					cout << "Publisher: " << list->find(i)->item.book[j].publisher << endl;
+					cout << "Year Published: " << list->find(i)->item.book[j].yearPublished << endl;
+					cout << "ISBN: " << list->find(i)->item.book[j].ISBN << endl;
+					cout << "Call Number: " << list->find(i)->item.book[j].callNum << endl;
+					cout << "Borrow Date: " << list->find(i)->item.book[j].borrow.day << '/' << list->find(i)->item.book[j].borrow.month << '/' << list->find(i)->item.book[j].borrow.year << endl;
+					cout << "Due Date: " << list->find(i)->item.book[j].due.day << '/' << list->find(i)->item.book[j].due.month << '/' << list->find(i)->item.book[j].due.year << endl;
+					cout << "Fine: " << list->find(i)->item.book[j].fine << endl;
+				}
+				cout << "*" << setw(100) << setfill('*');
+
+			}
+			break;
+		case 2:
+			
+			for (int i = 1; i <= list->count; i++) {
+				cout << endl << "STUDENT " + i << endl;
+				cout << endl << "Name:  " << list->find(i)->item.name << endl;
+				cout << "Id:  " << list->find(i)->item.id << endl;
+				cout << "Course:  " << list->find(i)->item.course << endl;
+				cout << "Phone No:  " << list->find(i)->item.phone_no << endl;
+				cout << "Total Fine:  RM" << list->find(i)->item.calculateTotalFine() << fixed << setprecision(2) << endl << endl;
+				cout << "*" << setw(100) << setfill('*') << endl;
+			}
+			break;
+		}
+		break;
+	}
+}
+
 		
 	
 	
